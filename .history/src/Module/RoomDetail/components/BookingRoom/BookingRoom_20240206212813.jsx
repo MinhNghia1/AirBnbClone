02430c1caa550379d3402c9842e305907fa98e8f
unknown <phtnhvan8624@gmail.com>
2024-02-07 01:26:45 +0700

    fix modal bookingRoom and handle valid DateBooked

---
+++ .history/src/Module/RoomDetail/components/BookingRoom/BookingRoom_20240206212813.jsx
@@ -0,0 +1,344 @@
+import React, { useEffect, useState, useRef } from "react";
+import styled from "./BookingRoom.module.scss";
+import { Box, Typography, Modal, Stack, Snackbar, Alert } from "@mui/material";
+import StarIcon from "@mui/icons-material/Star";
+import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange } from "react-date-range";
+import dayjs from "dayjs";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { bookingRoom, getRoomisBooked } from "../../../../Apis/room";
+import { ModalContent } from "../../../../Components/Modal/index";
+import { ButtonCustom, ButtonMain } from "../../../../Components/Button/Button";
+import { useSelector } from "react-redux";
+import Swal from 'sweetalert2';
+export default function BookingRoom({
+  roomId,
+  price,
+  averageRating,
+  totalReviews,
+}) {
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  const { data: ListOfRoomIsBooked = [] } = useQuery({
+    queryKey: ["isBooked"],
+    queryFn: getRoomisBooked,
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectionRange, setSelectionRange] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+  const refCalendar = useRef(null);
+  const queriClient = useQueryClient();
+  const [count, setCount] = useState(1);
+  const [countDays, setCountDays] = useState(0);
+  const [openSuccess, setOpenSuccess] = useState(false);
+  const [openStack, setOpenStack] = useState(false);
+
+  const { mutate } = useMutation({
+    mutationFn: (value) => {
+      const valueForm = {
+        id: currentUser.user.id,
+        maPhong: roomId,
+        ngayDen: dayjs(selectionRange.startDate).format(),
+        ngayDi: dayjs(selectionRange.endDate).format(),
+        soLuongKhach: count,
+        maNguoiDung: currentUser.user.id,
+      };
+      return bookingRoom(valueForm);
+    },
+    onSuccess: () => {
+      setOpenStack(true);
+      setOpenSuccess(false);
+      setTimeout(() => {
+        queriClient.invalidateQueries({ queryKey: ["isBooked"] });
+      }, 500);
+    },
+  });
+
+  const isBookingValid = () => {
+    const isRoomAlreadyBooked = ListOfRoomIsBooked.some((booking) => {
+      const bookingStartDate = dayjs(booking.ngayDen);
+      const bookingEndDate = dayjs(booking.ngayDi);
+      const selectionStartDate = dayjs(selectionRange.startDate);
+      const selectionEndDate = dayjs(selectionRange.endDate);
+  
+      const isSelectionWithinBookingRange = selectionStartDate.isSameOrAfter(bookingStartDate) && selectionStartDate.isSameOrBefore(bookingEndDate);
+      const isSelectionEndWithinBookingRange = selectionEndDate.isSameOrAfter(bookingStartDate) && selectionEndDate.isSameOrBefore(bookingEndDate);
+  
+      const isBookingWithinSelectionRange = bookingStartDate.isSameOrAfter(selectionStartDate) && bookingStartDate.isSameOrBefore(selectionEndDate);
+      const isBookingEndWithinSelectionRange = bookingEndDate.isSameOrAfter(selectionStartDate) && bookingEndDate.isSameOrBefore(selectionEndDate);
+  
+      const isBookingStartBeforeSelectionStart = bookingStartDate.isBefore(selectionStartDate);
+      const isBookingEndAfterSelectionEnd = bookingEndDate.isAfter(selectionEndDate);
+  
+      const isBookingInRange = (
+        (isSelectionWithinBookingRange && isSelectionEndWithinBookingRange) ||
+        (isBookingWithinSelectionRange && isBookingEndWithinSelectionRange) ||
+        (isBookingStartBeforeSelectionStart && isBookingEndAfterSelectionEnd)
+      );
+  
+      const sameUser = booking.maNguoiDung === currentUser.user.id;
+      const sameCount = booking.soLuongKhach === count;
+  
+      return isBookingInRange && sameUser && sameCount;
+    });
+  
+    console.log(isRoomAlreadyBooked);
+    return isRoomAlreadyBooked;
+  }; 
+  
+  
+  
+
+const handleBooking = () => {
+  if (isBookingValid()) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Đặt phòng không thành công!',
+      text: 'Phòng đã được đặt hoặc thông tin đặt phòng không hợp lệ.'
+    });
+  } else {
+    setOpenSuccess(true);
+    Swal.fire({
+      icon: 'success',
+      title: 'Đặt phòng thành công!'
+    });
+  }
+};
+
+
+  useEffect(() => {
+    document.addEventListener("click", clickOutSide, true);
+    document.addEventListener("keydown", escKey, true);
+
+    return () => {
+      document.removeEventListener("click", clickOutSide, true);
+      document.removeEventListener("keydown", escKey, true);
+    };
+  }, []);
+
+  const clickOutSide = (e) => {
+    if (refCalendar.current && !refCalendar.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const escKey = (e) => {
+    if (e.key === "Escape") {
+      setIsOpen(false);
+    }
+  };
+
+  const handleSelect = (ranges) => {
+    setSelectionRange(ranges.selection);
+    const ngayDau = new Date(ranges.selection.startDate);
+    const ngayCuoi = new Date(ranges.selection.endDate);
+    setCountDays(Math.abs((ngayCuoi - ngayDau) / (1000 * 60 * 60 * 24)));
+  };
+
+  const handleCountPlus = () => {
+    setCount(count + 1);
+  };
+
+  const handleCountMinus = () => {
+    if (count === 1) return;
+    setCount(count - 1);
+  };
+
+  const disabledRanges = ListOfRoomIsBooked.map((day) => {
+    return { ngayDen: day.ngayDen, ngayDi: day.ngayDi };
+  });
+
+  function isDateInDisabledRange(date) {
+    return disabledRanges.some((disabledRange) => {
+      const start = new Date(disabledRange.ngayDen);
+      const end = new Date(disabledRange.ngayDi);
+      return date >= start && date <= end;
+    });
+  }
+
+  const handleCloseStack = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenStack(false);
+  };
+
+  return (
+    <div className={styled.bookingRoom}>
+      <div className={styled.bookingContent}>
+        <div className={styled.bookingTitle}>
+          <Typography sx={{ fontSize: "25px" }}>
+            ${price}
+            <span className={styled.priceOfNight}>/ đêm</span>
+          </Typography>
+          <p className={styled.averageRating}>
+            <StarIcon sx={{ marginRight: "3px", fontSize: "17px" }} />{" "}
+            {averageRating} ·
+            <span className={styled.totalFeedback}>
+              {totalReviews} đánh giá
+            </span>
+          </p>
+        </div>
+        <div className={styled.bookingDetail}>
+          <Box
+            sx={{
+              display: "flex",
+              border: "1px solid #B0B0B0",
+              padding: "5px",
+              borderTopRightRadius: "10px",
+              borderTopLeftRadius: "10px",
+            }}
+          >
+            {/* Calendar  */}
+            <div onClick={() => setIsOpen(true)} className={styled.dateRight}>
+              <Typography sx={{ fontSize: "11px", color: "#222222" }}>
+                NHẬN PHÒNG
+              </Typography>
+              <Typography sx={{ fontSize: "14px", color: "#717171" }}>
+                {dayjs(selectionRange.startDate).format("DD/MM/YYYY")}
+              </Typography>
+            </div>
+            <div onClick={() => setIsOpen(true)} className={styled.dateLeft}>
+              <Typography sx={{ fontSize: "11px", color: "#222222" }}>
+                TRẢ PHÒNG
+              </Typography>
+              <Typography sx={{ fontSize: "14px", color: "#717171" }}>
+                {dayjs(selectionRange.endDate).format("DD/MM/YYYY")}
+              </Typography>
+            </div>
+          </Box>
+          {isOpen && (
+            <div ref={refCalendar} className={styled.wrapCalendar}>
+              <DateRange
+                ranges={[selectionRange]}
+                onChange={handleSelect}
+                editableDateInputs={true}
+                moveRangeOnFirstSelection={false}
+                months={2}
+                direction="horizontal"
+                rangeColors={["#f33e5b"]}
+                dateDisplayFormat="dd/MM/yyyy"
+                disabledDay={(date) => isDateInDisabledRange(date)}
+              />
+            </div>
+          )}
+
+          {/* Customer  */}
+          <div className={styled.customerItem}>
+            <div className={styled.customerRight}>
+              <Typography sx={{ fontSize: "11px", color: "#222222" }}>
+                KHÁCH
+              </Typography>
+              <Typography sx={{ fontSize: "14px", color: "#717171" }}>
+                {count} khách
+              </Typography>
+            </div>
+            <div className={styled.customerLeft}>
+              <Typography onClick={handleCountPlus} sx={{ fontSize: "11px" }}>
+                <AddCircleOutlineOutlinedIcon />
+              </Typography>
+              <Typography sx={{ fontSize: "18px", color: "#717171" }}>
+                {count}
+              </Typography>
+              <Typography onClick={handleCountMinus} sx={{ fontSize: "11px" }}>
+                <RemoveCircleOutlineOutlinedIcon />
+              </Typography>
+            </div>
+          </div>
+          <div className={styled.btnBookingItem}>
+            <button
+              onClick={handleBooking}
+              type="submit"
+              className={styled.btnBooking}
+            >
+              Đặt phòng
+            </button>
+          </div>
+          <div className={styled.priceBeforeBooking}>
+            <Typography sx={{ textDecoration: "underline", fontSize: "15px" }}>
+              ${price} x {countDays} đêm
+            </Typography>
+            <Typography sx={{ textDecoration: "underline", fontSize: "15px" }}>
+              {price * countDays}$
+            </Typography>
+          </div>
+          <div className={styled.totalPrice}>
+            <Typography sx={{ fontSize: "20px", fontWeight: "500" }}>
+              Tổng tiền
+            </Typography>
+            <Typography sx={{ fontSize: "20px", fontWeight: "500" }}>
+              {price * countDays}$
+            </Typography>
+          </div>
+        </div>
+      </div>
+      <Modal
+        open={openSuccess}
+        onClose={() => {
+          setOpenSuccess(false);
+        }}
+        sx={{
+          position: "fixed",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          backgroundColor: " rgba(0, 0, 0, 0.6)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          opacity: "999",
+        }}
+      >
+        <ModalContent>
+          <img
+            style={{ width: "120px", marginTop: "10px" }}
+            src="https://i.pinimg.com/564x/e5/b9/81/e5b98110fcd62d6ebe0e636262170175.jpg"
+            alt="confirm"
+          />
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              marginBottom: "40px",
+              color: " #f43f5e",
+            }}
+          >
+            Bạn có chắc chắn đặt phòng ?
+          </Typography>
+
+          <ButtonMain onClick={() => mutate()}>Xác nhận</ButtonMain>
+          <ButtonCustom
+            onClick={() => {
+              setOpenSuccess(false);
+            }}
+          >
+            Hủy
+          </ButtonCustom>
+        </ModalContent>
+      </Modal>
+      <Stack spacing={2} sx={{ width: "300%" }}>
+        <Snackbar
+          open={openStack}
+          autoHideDuration={1500}
+          onClose={handleCloseStack}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        >
+          <Alert
+            onClose={handleCloseStack}
+            severity="success"
+            sx={{ width: "300%" }}
+          >
+            Đặt phòng thành công!
+          </Alert>
+        </Snackbar>
+      </Stack>
+    </div>
+  );
+}
---
+++ .history/src/Module/RoomDetail/components/Feedback/Feedback_20240206193101.jsx
@@ -0,0 +1,165 @@
+import React, { useEffect } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { getFeedbacks, postFeedbacks } from "../../../../Apis/feedback";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Rating,
+  TextField,
+  Typography,
+} from "@mui/material";
+import styled from "./Feedback.module.scss";
+import dayjs from "dayjs";
+import { Controller, useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
+export default function Feedback({ roomId, onGetFeedbacks }) {
+  const queryClient = useQueryClient();
+  const currentUser = useSelector((state) => state.auth.currentUser);
+  // Thông tin user
+  const user = currentUser?.user;
+  // console.log(user,"dkfsjsdlf");
+  // Ngày comment
+  const dayComment = dayjs().format("ddd, DD MMM YYYY HH:mm:ss [GMT]");
+  // input form
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: setValue,
+  } = useForm({
+    defaultValues: {
+      noiDung: "",
+      saoBinhLuan: 0,
+    },
+  });
+
+  // Lấy feedbacks từ API
+  const { data: feedbacks = [] } = useQuery({
+    queryKey: ["comments"],
+    queryFn: () => getFeedbacks(roomId),
+  });
+
+  // Gọi API post comments
+  const { mutate: onSuccess } = useMutation({
+    mutationFn: (valuesForm) => {
+      const values = {
+        id: user.id,
+        maPhong: roomId,
+        maNguoiBinhLuan: user.id,
+        ngayBinhLuan: dayComment,
+        noiDung: valuesForm.noiDung,
+        saoBinhLuan: valuesForm.saoBinhLuan,
+      };
+      return postFeedbacks(values);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments"] });
+      setValue("noiDung", "");
+      setValue("saoBinhLuan", 0);
+    },
+  });
+  useEffect(() => {
+    if (feedbacks.length > 0) {
+      onGetFeedbacks(feedbacks);
+    }
+  }, [feedbacks, onGetFeedbacks]);
+
+  return (
+    <div className={styled.feedback}>
+       <Container>
+        {feedbacks.map((feedback, index) => (
+          <div key={index} item md={6} sx={{ marginBottom: "50px"}}>
+            <div className={styled.feedbackItem}>
+              <img
+                style={{ borderRadius: "10px" }}
+                width={50}
+                height={50}
+                src={feedback.avatar || "https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-1/404931285_3568833133329150_7685244699268670578_n.jpg?stp=cp6_dst-jpg_p320x320&_nc_cat=104&ccb=1-7&_nc_sid=5740b7&_nc_eui2=AeFWqGgOizBCrBmF1XQWrkhNb3ARluray7RvcBGW6trLtOH0lpff907-sjIbSnZuzzOU2LN9UlWFyAV4UT6XYPbj&_nc_ohc=GrjU8Xe0guoAX8jD82j&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBDif0Q4OzoxO9mL1nOLB2ZcGNxeZCDROfd1bYqQvdobg&oe=65BA636B"}
+                alt=""
+              />
+              <div className={styled.feedbackDetail}>
+                <Typography
+                  sx={{
+                    fontSize: "18px",
+                  }}
+                >
+                  {feedback.tenNguoiBinhLuan}
+                </Typography>
+                <Typography sx={{ fontSize: "15px", color: "gray" }}>
+                  {dayjs(feedback.ngayBinhLuan).format("MM/YYYY")}
+                </Typography>
+                <Rating
+                  name="read-only"
+                  readOnly
+                  value={feedback.saoBinhLuan}
+                  sx={{ fontSize: "15px" }}
+                />
+              </div>
+            </div>
+            <div className={styled.feedbackDesc}>
+              <Typography>{feedback.noiDung}</Typography>
+            </div>
+          </div>
+        ))}
+      </Container>
+      <Button
+        sx={{ fontWeight: "500", fontSize: "16px", marginBottom: "50px" }}
+        variant="outlined"
+      >
+        Hiển thị tất cả đánh giá
+      </Button>
+
+      {/* Kiểm tra User  */}
+      {user && (
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSuccess)}
+          sx={{
+            "& .MuiTextField-root": { m: 1 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div className={styled.addComment}>
+            <img
+              style={{ borderRadius: "10px" }}
+              width={50}
+              height={50}
+              src={user.avatar || "https://scontent.fsgn5-3.fna.fbcdn.net/v/t39.30808-1/404931285_3568833133329150_7685244699268670578_n.jpg?stp=cp6_dst-jpg_p320x320&_nc_cat=104&ccb=1-7&_nc_sid=5740b7&_nc_eui2=AeFWqGgOizBCrBmF1XQWrkhNb3ARluray7RvcBGW6trLtOH0lpff907-sjIbSnZuzzOU2LN9UlWFyAV4UT6XYPbj&_nc_ohc=GrjU8Xe0guoAX8jD82j&_nc_ht=scontent.fsgn5-3.fna&oh=00_AfBDif0Q4OzoxO9mL1nOLB2ZcGNxeZCDROfd1bYqQvdobg&oe=65BA636B"}
+              alt=""
+            />
+            <TextField
+              id="comment"
+              label="Bình luận"
+              multiline
+              fullWidth
+              rows={4}
+              name="noiDung"
+              {...register("noiDung")}
+            />
+          </div>
+          <div className={styled.btnAddComment}>
+            <Typography component="legend" sx={{ paddingLeft: "5px" }}>
+              Đánh giá sao
+            </Typography>
+            <Controller
+              name="saoBinhLuan"
+              control={control}
+              render={({ field }) => (
+                <Rating
+                  sx={{ paddingBottom: "15px", fontSize: "30px" }}
+                  {...field}
+                />
+              )}
+            />
+            <Button sx={{ display: "block" }} variant="contained" type="submit">
+              Thêm bình luận
+            </Button>
+          </div>
+        </Box>
+      )}
+    </div>
+  );
+}
